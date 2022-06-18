ec27f21bc737d0ec8543605a9d8b4f3176104b42
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-06-18 09:40:30 +0300

    ft: add a test for admin

--- hood/models.py
+++ hood/models.py
@@ -1,14 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Admin(models.Model):
-    username = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50)
-    password = models.CharField(max_length=50)
-    phone = models.CharField(max_length=50)
-    def __str__(self):
-        return self.name
+    username = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+    confirm_password = models.CharField(max_length=100, default=password)
+    phone = models.CharField(max_length=100)
+
+    def __repr__(self):
+        return self.username
+    
 
 class Neighbourhood(models.Model):
     name = models.CharField(max_length=50)
--- hood/tests.py
+++ hood/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import *
 
 # Create your tests here.
+
+class AdminTest(TestCase):
+    def setUp(self):
+        self.admin = Admin.objects.create(username='testuser', email = 'test@gmail.com', password='12345', confirm_password='12345', phone='0712345678')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.admin, Admin))
