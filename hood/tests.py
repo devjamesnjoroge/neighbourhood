97d2375b8b03b9d9d4e1d76bd3eca4c9a54b1e56
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-06-19 12:24:54 +0300

    ft: add tests for business

--- hood/tests.py
+++ hood/tests.py
@@ -13,7 +13,7 @@ class AdminTest(TestCase):
 
     def test_save_admin(self):
         self.admin.save_admin()
-        self.assertTrue(isinstance(self.admin, Admin))
+        self.assertTrue(len(Admin.objects.all()) > 0)
 
 class NeighbourhoodTest(TestCase):
     def setUp(self):
@@ -25,11 +25,11 @@ class NeighbourhoodTest(TestCase):
 
     def test_save_neighbourhood(self):
         self.neighbourhood.save_neighbourhood()
-        self.assertTrue(isinstance(self.neighbourhood, Neighbourhood))
+        self.assertTrue(len(Neighbourhood.objects.all())>0)
 
     def test_delete_neighbourhood(self):
         self.neighbourhood.delete_neighbourhood()
-        self.assertTrue(isinstance(self.neighbourhood, Neighbourhood))
+        self.assertTrue(len(Neighbourhood.objects.all())==0)
 
     def test_find_neighbourhood(self):
         self.neighbourhood.find_neighbourhood()
@@ -42,3 +42,33 @@ class NeighbourhoodTest(TestCase):
     def test_search_neighbourhood(self):
         self.neighbourhood.search_neighbourhood()
         self.assertTrue(isinstance(self.neighbourhood, Neighbourhood))
+
+class BusinessTest(TestCase):
+    def setUp(self):
+        self.admin = Admin.objects.create(username='testuser', email = 'test@gmail.com', password='12345', confirm_password='12345')
+        self.neighbourhood = Neighbourhood.objects.create(name='test', location='test', occupants=0, admin=self.admin)
+        self.user = User.objects.create(name='test', email = 'test@gmail.com', password='12345', neighbourhood=self.neighbourhood)
+        self.business = Business.objects.create(business_name='test', business_email = 'business@gmail.com', user = self.user, neighbourhood=self.neighbourhood)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.business, Business))
+
+    def test_save_business(self):
+        self.business.save_business()
+        self.assertTrue(len(Business.objects.all())>0)
+
+    def test_delete_business(self):
+        self.business.delete_business()
+        self.assertTrue(len(Business.objects.all())==0)
+
+    def test_find_business(self):
+        self.business.find_business()
+        self.assertTrue(isinstance(self.business, Business))
+
+    def test_update_business(self):
+        self.business.update_business()
+        self.assertTrue(isinstance(self.business, Business))
+
+    def test_search_business(self):
+        self.business.search_business()
+        self.assertTrue(isinstance(self.business, Business))
